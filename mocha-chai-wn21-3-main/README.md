b066fee7a31f6de929930ae978a4d4d7562c4b64
yosuof ibrahim <72518231+yosuof206@users.noreply.github.com>
2021-03-20 16:43:55 -0700

    Add files via upload

---
+++ mocha-chai-wn21-3-main/README.md
@@ -0,0 +1,2 @@
+# mocha-chai-wn21-2
+Demos unit test in Mocha and Chai
---
+++ mocha-chai-wn21-3-main/index.js
@@ -0,0 +1,4 @@
+var Calculator = require("./Calculator.js");//.js is optional
+var calc = new Calculator();
+let result = calc.add(2,1);
+console.log(result);
